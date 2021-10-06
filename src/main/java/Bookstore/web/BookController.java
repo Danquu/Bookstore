f6554472bda32139adad73ecc2c12177d2aa3f27
Danquu <bgp037@myy.haaga-helia.fi>
2021-10-06 10:49:52 +0300

    C5.2

--- src/main/java/Bookstore/web/BookController.java
+++ src/main/java/Bookstore/web/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,14 +39,21 @@ public class BookController {
 		return "booklist";
 	}
 	
+	//REST
 	@RequestMapping(value="/books", method = RequestMethod.GET)
 	public @ResponseBody List<Book> bookListRest() {
 		return (List<Book>) repository.findAll();
 	}
 	
+	//REST
 	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
 	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
 		return repository.findById(bookId);
+	}
+	//REST
+	@RequestMapping(value="/books", method = RequestMethod.POST)
+	public @ResponseBody Book saveBookRest(@RequestBody Book book) {
+		return repository.save(book);
 	}
 		
 	@RequestMapping(value ="/addbook")
