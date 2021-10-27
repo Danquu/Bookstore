199666572dbe63a2147cf0b645bdc1c0398c265d
Danquu <bgp037@myy.haaga-helia.fi>
2021-10-27 15:07:15 +0300

    C7.1 aaa

--- SpringBootJDBC/src/main/resources/inserttestdata.sql
+++
@@ -1,9 +0,0 @@
--- aja tämä scripti tietokantapalvelimella, jos haluat kantaan testidataa
-
-INSERT INTO student (student_id, first_name, last_name)
-VALUES (1, "John", "West");
-INSERT INTO student (student_id, first_name, last_name)
-VALUES (2, "Mike", "Mars");
-INSERT INTO student (student_id, first_name, last_name)
-VALUES (3, "Kate", "Johnson");
-    
--- SpringBootJDBC/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
--- aja tämä scripti tietokantapalvelimella 
-
-CREATE TABLE IF NOT EXISTS student (
-    student_id int(5) NOT NULL AUTO_INCREMENT,
-    first_name varchar(50) DEFAULT NULL,
-    last_name varchar(50) DEFAULT NULL,
-    PRIMARY KEY(student_id)
-    );
